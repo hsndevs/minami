3df3f87d06ee84945ff0577eaf89a11d59f5d75f
Hasan Tareq <hsntareq@gmail.com>
2025-06-16 22:03:30 +0600

    16 June, 2025 edit enqueue

--- inc/Blocks/Patterns.php
+++ inc/Blocks/Patterns.php
@@ -40,6 +40,26 @@ class Patterns {
 	 */
 	public function minami_register_block_patterns() {
 		// Register banner patterns.
+		register_block_pattern('minami/minami-page-header', [
+			'title'   => __('Minami Page Header', 'minami'),
+			'content' => '<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"primary","layout":{"type":"default"}} -->
+<div class="wp-block-group has-primary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:post-title {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} /-->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:media-text {"mediaId":315,"mediaLink":"http://dminami.local/home-2/","mediaType":"image","mediaWidth":19,"verticalAlignment":"center","className":"min-image-text-sm","style":{"layout":{"selfStretch":"fit","flexSize":null}},"fontSize":"extra-small"} -->
+<div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-center min-image-text-sm has-extra-small-font-size" style="grid-template-columns:19% auto"><figure class="wp-block-media-text__media"><img src="' . esc_url(get_template_directory_uri() . '/assets/images/home.png') . '" alt="" class="wp-image-315 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…"} -->
+<p><strong><a href="/">Home</a></strong></p>
+<!-- /wp:paragraph --></div></div>
+<!-- /wp:media-text -->
+
+<!-- wp:quote -->
+<blockquote class="wp-block-quote"><!-- wp:post-title {"level":6} /--></blockquote>
+<!-- /wp:quote --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->',
+		]);
+
+		// Register banner patterns.
 		register_block_pattern('minami/minami-banner', [
 			'title'   => __('Minami Banner', 'minami'),
 			'content' => '<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"background":{"backgroundImage":{"url":"' . esc_url(get_template_directory_uri() . '/assets/images/namba-square.png') . '","id":119,"source":"file","title":"namba-square"},"backgroundSize":"cover","backgroundAttachment":"scroll","backgroundPosition":"50% 50%"},"dimensions":{"minHeight":"600px"}},"layout":{"type":"default"}} --><div class="wp-block-group" style="min-height:600px;margin-top:0;margin-bottom:0"></div><!-- /wp:group -->',
