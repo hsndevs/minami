dd5be2d4add66a7a1c04ec63bd55e7cdeffe8d30
Hasan Tareq <hsntareq@gmail.com>
2025-06-16 20:55:09 +0600

    Banner added

--- patterns/header.php
+++ patterns/header.php
@@ -8,11 +8,11 @@
  */
 ?>
 <!-- wp:group {"style":{"position":{"type":""},"border":{"bottom":{"width":"1px"}}},"layout":{"type":"default"}} -->
-<div class="wp-block-group" style="border-bottom-width:1px"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","layout":{"type":"constrained","contentSize":"1352px"}} -->
+<div class="wp-block-group" style="border-bottom-width:1px"><!-- wp:group {"metadata":{"name":"Header Top"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"margin":{"top":"0","bottom":"0"}}},"backgroundColor":"primary","layout":{"type":"constrained","contentSize":"1352px"}} -->
 <div class="wp-block-group has-primary-background-color has-background" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
 <div class="wp-block-group"><!-- wp:media-text {"mediaId":20,"mediaLink":"http://dminami.local/image/","mediaType":"image","mediaWidth":15,"verticalAlignment":"center","className":"min-image-text-sm","fontSize":"extra-small"} -->
 <div class="wp-block-media-text is-stacked-on-mobile is-vertically-aligned-center min-image-text-sm has-extra-small-font-size" style="grid-template-columns:15% auto"><figure class="wp-block-media-text__media"><img src="<?php echo get_theme_file_uri( '/assets/images/location1.webp' ); ?>" alt="" class="wp-image-20 size-full"/></figure><div class="wp-block-media-text__content"><!-- wp:paragraph {"placeholder":"Content…"} -->
-<p><strong>Namba Sennichimae, Chuo-ku, Osaka</strong></p>
+<p><strong><a href="https://maps.app.goo.gl/9mFKsbtwNbp97XTc9" target="_blank" rel="noreferrer noopener">Namba Sennichimae, Chuo-ku, Osaka</a></strong></p>
 <!-- /wp:paragraph --></div></div>
 <!-- /wp:media-text -->
 
@@ -32,10 +32,10 @@
 <!-- /wp:group --></div>
 <!-- /wp:group -->
 
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black","textColor":"white","layout":{"type":"constrained","contentSize":"1352px"}} -->
+<!-- wp:group {"metadata":{"name":"Header Bottom"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"black","textColor":"white","layout":{"type":"constrained","contentSize":"1352px"}} -->
 <div class="wp-block-group has-white-color has-black-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--20);padding-bottom:var(--wp--preset--spacing--20)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group"><!-- wp:image {"lightbox":{"enabled":false},"id":98,"width":"400px","sizeSlug":"full","linkDestination":"custom"} -->
-<figure class="wp-block-image size-full is-resized"><img src="<?php echo get_theme_file_uri( '/assets/images/logo-white.webp' ); ?>" alt="" class="wp-image-98" style="width:400px"/></figure>
+<div class="wp-block-group"><!-- wp:image {"lightbox":{"enabled":false},"id":96,"width":"400px","height":"auto","sizeSlug":"full","linkDestination":"custom","style":{"color":{"duotone":["#000000","rgb(255, 255, 255)"]}}} -->
+<figure class="wp-block-image size-full is-resized"><img src="<?php echo get_theme_file_uri( '/assets/images/logo-green.png' ); ?>" alt="" class="wp-image-96" style="width:400px;height:auto"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:navigation {"ref":27} /--></div>
