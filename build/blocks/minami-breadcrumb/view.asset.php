072663c9e06ca8549770b78e80349930cd9addc6
Hasan Tareq <hsntareq@gmail.com>
2025-07-10 13:33:42 +0600

    03 July, 2025: Gitignore update

---
+++ build/blocks/minami-breadcrumb/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'cbf8bd57e847e59a7bf5');
---
+++ build/blocks/minami-history/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3aa6dd3680559f652b21');
