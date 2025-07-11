698b380a1f530b92aa4c9a8657909f8455396136
Hasan Tareq <hsntareq@gmail.com>
2025-07-11 07:09:37 +0600

    11 July, 2025: History block updated

--- build/blocks/history/index.asset.php
+++ build/blocks/history/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '582d2bc5c456777cb131');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '43ea7a4944536738b191');
