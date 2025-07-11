d5249727cb5f12688b8ced692519d93a64ae59ba
Hasan Tareq <hsntareq@gmail.com>
2025-07-11 08:48:32 +0600

    11 July, 2025: Input text alignment of history block

--- build/blocks/history/index.asset.php
+++ build/blocks/history/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '070870d736f9e6ce0dbf');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '0ca6242a7a7374d04b36');
