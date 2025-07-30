5f4fb435577ce9da84c0df4ee532e4752c6f5726
Hasan Tareq <hsntareq@gmail.com>
2025-07-30 14:14:25 +0600

    Responsiveness navigation menu

--- build/blocks/navigation-menu/index.asset.php
+++ build/blocks/navigation-menu/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'b473dba91b49ec8b2ab9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => 'd7004dcd029aa36006ba');
